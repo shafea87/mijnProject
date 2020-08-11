17d6859ce44397dc717c99de96281b41f8bfd6ba
shafea1987 <m.s.alsalkini@gmail.com>
2020-08-11 06:47:15 +0200

    js fils

---
+++ js/EersteProject/js/app.js
@@ -0,0 +1,9 @@
+//Wat is je geslacht?
+function mor(){
+document.body.style.backgroundColor = "lightblue";
+}
+function vor(){
+  document.body.style.backgroundColor = "HotPink";
+}
+
+//Wat is jaar geboren?
---
+++ js/TweedeProject/app.js
@@ -0,0 +1,28 @@
+function nem(){
+  document.getElementById("mn").innerHTML = "maandag";
+  document.body.style.backgroundColor = "lightblue"
+}
+function ned(){
+  document.getElementById("mn").innerHTML = "dinsdag";
+  document.body.style.backgroundColor = "lightgreen"
+}
+function neww(){
+  document.getElementById("mn").innerHTML = "woensdag";
+  document.body.style.backgroundColor = "Coral"
+}
+function nedd(){
+  document.getElementById("mn").innerHTML = "dinsdag";
+  document.body.style.backgroundColor = "DarkTurquoise"
+}
+function nev(){
+  document.getElementById("mn").innerHTML = "vrijdag";
+  document.body.style.backgroundColor = "GreenYellow"
+}
+function nes(){
+  document.getElementById("mn").innerHTML = "zaterdag";
+  document.body.style.backgroundColor = "lightgreen"
+}
+function nez(){
+  document.getElementById("mn").innerHTML = "zondag";
+  document.body.style.backgroundColor = "MediumAquaMarine"
+}
