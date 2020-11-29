6d66198daae3a3f85cc81160ac69c9691eb128a5
shafea1987 <m.s.alsalkini@gmail.com>
2020-11-29 20:24:18 +0100

    top

--- wordpress/wordpress/wp-content/themes/boost-biz/assets/js/metabox.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(){jQuery("#boost-biz-ui-tabs").tabs()});
--- wordpress/wordpress/wp-content/themes/real-estate-salient/inc/loader.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
-* Load all the modules
-*/
-
-require_once get_template_directory() . '/inc/breadcrumbs/breadcrumbs.php';
